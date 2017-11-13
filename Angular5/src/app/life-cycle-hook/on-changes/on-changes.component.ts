0f40253c6d3d953c6dbe6f84d873726c49d04137
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-11-13 00:45:52 -0500

    on changes component created

---
+++ Angular5/src/app/life-cycle-hook/on-changes/on-changes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-on-changes',
+  templateUrl: './on-changes.component.html',
+  styleUrls: ['./on-changes.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class OnChangesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
