a1d22736a5324e305d6c39e83cc21e5c12421e3a
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-11-17 01:33:45 -0500

    test added fro the pipe

--- Angular5/src/app/components/number-to-weather/number-to-weather.component.spec.ts
+++ Angular5/src/app/components/number-to-weather/number-to-weather.component.spec.ts
@@ -1,14 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NumberToWeatherComponent } from './number-to-weather.component';
+import { NumberToWeatherPipe } from '../../pipes/number-to-weather.pipe';
 
 describe('NumberToWeatherComponent', () => {
   let component: NumberToWeatherComponent;
   let fixture: ComponentFixture<NumberToWeatherComponent>;
-
+  const pipe = new NumberToWeatherPipe();
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NumberToWeatherComponent ]
+      declarations: [ 
+        NumberToWeatherComponent,
+        NumberToWeatherPipe ]
     })
     .compileComponents();
   }));
@@ -22,4 +25,24 @@ describe('NumberToWeatherComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('It should map 0 to Snowey', () => {
+    expect(pipe.transform(0)).toBe('Snowey');
+  });
+
+  it('It should map 1 to cloudy', () => {
+    expect(pipe.transform(1)).toBe('cloudy');
+  });
+
+  it('It should map numbers or words other than 0 or 1 to their default', () => {
+    let textToTransform = 'Test pipe';
+    expect(pipe.transform(textToTransform)).toBe(textToTransform);
+  });
+
+  // it('should fail', () => {
+  //   expect(2).toBe(3);
+  // });
+
+
 });
